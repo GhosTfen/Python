bdc473d09e095aab20cdb21af54c9847956d8261
Ghost <73184277+GhosTfen@users.noreply.github.com>
2020-11-04 21:58:39 +0300

    Delete 1.1.py

--- 1.1.py
+++
@@ -1,4 +0,0 @@
-import math
-x = int (input("Введите значение с клавиатуры"))
-y = int (input("Введите значение с клавиатуры"))
-print ("Результат - ", (math.pow (2, -x)-math.cos(x)+math.sin( 2*x*y)))
