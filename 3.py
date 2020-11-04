38586a2663f61aeaa26123140fc8ba6e17bfaf15
Ghost <73184277+GhosTfen@users.noreply.github.com>
2020-11-04 16:46:17 +0300

    Delete 3.py

--- 3.py
+++
@@ -1,9 +0,0 @@
-
-a=int (input("Введите значение для a="))
-b=int (input("Введите значение для b="))
-c=int (input("Введите значение для c="))
-d=int (input("Введите значение для d="))
-if ((a==c) and (b==d)):
-   print ("True")
-else :
-   print ("False")
