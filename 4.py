ff5cd73672f6ac0daced778189122d061f1a6532
Ghost <73184277+GhosTfen@users.noreply.github.com>
2020-11-04 21:58:11 +0300

    Delete 4.py

--- 4.py
+++
@@ -1,6 +0,0 @@
-x = int (input("Введите значение с клавиатуры"))
-y = int (input("Введите значение с клавиатуры"))
-if (y<=x+4) and (y<=x+4) and (y>=0 and x>=-4 and x<=4):
-    print ("True")
-else:
-    print ("False")
